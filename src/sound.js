83cbd4d0968f6aebe235367749a605089a8bfae4
jigooo <h00kk@naver.com>
2023-02-08 15:57:53 +0900

    0208

--- src/sound.js
+++ src/sound.js
@@ -1,7 +1,7 @@
 "use strict";
 const carrotSound = new Audio("./sound/carrot_pull.mp3");
 const alertSound = new Audio("./sound/alert.wav");
-const bgSound = new Audio("./sound/bg.mp3");
+const bgSound = new Audio("./sound/bg02.mp3");
 const bugSound = new Audio("./sound/bug_pull.mp3");
 const winSound = new Audio("./sound/game_win.mp3");
 
